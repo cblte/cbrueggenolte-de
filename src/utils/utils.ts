c9b24d2e2d5dc681e6ab5db159ba3350fe5f3e6a
cblte <mail@cbrueggenolte.de>
2025-04-14 11:06:26 +0200

    Add cover image support to MDPage layout and update utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,20 +1,35 @@
-
 // Date formatting function
 export function formatDate(dateString: string): string {
-    if (!dateString) return "";
+  if (!dateString) return "";
 
-    const date = new Date(dateString);
-    return `${date.getDate()}. ${date.toLocaleString("en-US", { month: "short" })}. ${date.getFullYear()}`;
-};
+  const date = new Date(dateString);
+  return `${date.getDate()}. ${date.toLocaleString("en-US", {
+    month: "short",
+  })}. ${date.getFullYear()}`;
+}
 
 export function createSlug(title: string): string {
-    // Create a slug from the title
-    const slug = title
-        .toLowerCase()
-        .replace(/[^a-z0-9\s-]/g, "") // Remove special characters
-        .trim()
-        .replace(/\s+/g, "-") // Replace spaces with hyphens
-        .replace(/-+/g, "-"); // Replace multiple hyphens with a single hyphen
+  // Create a slug from the title
+  const slug = title
+    .toLowerCase()
+    .replace(/[^a-z0-9\s-]/g, "") // Remove special characters
+    .trim()
+    .replace(/\s+/g, "-") // Replace spaces with hyphens
+    .replace(/-+/g, "-"); // Replace multiple hyphens with a single hyphen
 
-    return slug;
+  return slug;
+}
+
+interface Frontmatter {
+  cover: string;
+  coverDescription?: string;
+}
+
+export function getCoverImage(frontmatter: Frontmatter): string {
+  if (!frontmatter.cover) {
+    return ""; // or return a default image if desired
+  }
+
+  const altText = frontmatter.coverDescription || "Image cover"; // Default alt text
+  return `<img src="${frontmatter.cover}" alt="${altText}" title="${altText}" />`;
 }
