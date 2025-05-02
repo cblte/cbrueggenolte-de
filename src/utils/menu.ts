54b07f27baa231f9f237253e40578b871996fb0f
cblte <mail@cbrueggenolte.de>
2025-05-02 17:08:46 +0200

    feat: implement header and footer menus with dynamic navigation

---
+++ src/utils/menu.ts
@@ -0,0 +1,16 @@
+export const headerMenu = [
+  { name: 'Home', href: '/' },
+  { name: 'Über mich', href: '/about/' },
+  { name: 'Now', href: '/now/' },
+  { name: 'Blog', href: '/blog/' },
+  { name: 'Uses', href: '/uses/' },
+  { name: 'Kontakt', href: '/contact/' },
+];
+
+export const footerMenu = [
+  { name: 'Politik', href: '/politics/' },
+  { name: 'Mein Handbuch', href: '/manual-of-me/' },
+  { name: 'K.I.', href: '/ai/' },
+  { name: 'Lesezeichen', href: '/bookmarks/' },
+  // { name: 'Galerie', href: '/gallery/' },
+];
