56b443fd5f665c6ddf5d5816e2fb267022a79ab9
cblte <mail@cbrueggenolte.de>
2025-06-04 16:59:29 +0200

    feat: add Rubik font files and update body font class in Layout.astro

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Rubik', 'system-ui', 'sans-serif'],
+      },
       typography: () => ({
         DEFAULT: {
           css: {
